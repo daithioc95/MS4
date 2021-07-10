e677429077f29b82da06a255c4048e663537b7d2
daithioc95 <daithioc@outlook.com>
2021-07-10 19:23:35 +0000

    Updated webhook handler to handle profiles

--- checkout/models.py
+++ checkout/models.py
@@ -77,4 +77,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
+        return f'SKU {self.product.id} on order {self.order.order_number}'
