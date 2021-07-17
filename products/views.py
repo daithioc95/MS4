28939c1c997f0a50ad23d5c38afbb7cc26a1ea00
daithioc95 <daithioc@outlook.com>
2021-07-17 21:10:38 +0000

    Add similar artists to artist_detail page

--- products/views.py
+++ products/views.py
@@ -62,14 +62,22 @@ def all_artists(request):
     period = None
     edo_artists = {}
     meiji_artists = {}
+    other_artists = {}
     for x in Product.objects.all():
-        if x.Artist_Display_Name not in edo_artists:
-            if x.Period == "Edo period (1615–1868)":
-                edo_artists.update({x.Artist_Display_Name: x.image.url})
-        if x.Artist_Display_Name not in meiji_artists:
-            if x.Period == "Meiji period (1868–1912)":
-                meiji_artists.update({x.Artist_Display_Name: x.image.url})
-
+        try:
+            if x.Artist_Display_Name not in edo_artists:
+                if x.Period == "Edo period (1615–1868)":
+                    edo_artists.update({x.Artist_Display_Name: x.image.url})
+            if x.Artist_Display_Name not in meiji_artists:
+                if x.Period == "Meiji period (1868–1912)":
+                    meiji_artists.update({x.Artist_Display_Name: x.image.url})
+            if x.Artist_Display_Name not in other_artists:
+                if x.Period != "Edo period (1615–1868)":
+                    if x.Period != "Meiji period (1868–1912)":
+                        if x.Artist_Display_Name and x.image.url:
+                            other_artists.update({x.Artist_Display_Name: x.image.url})
+        except:
+            pass
     if 'Period' in request.GET:
             period = request.GET['Period']
 
@@ -77,6 +85,7 @@ def all_artists(request):
         'edo_artists': edo_artists,
         'meiji_artists': meiji_artists,
         'period': period,
+        'other_artists': other_artists,
     }
 
     return render(request, 'products/artists.html', context)
@@ -85,9 +94,17 @@ def all_artists(request):
 def artist_detail(request, artist_name):
     products = Product.objects.all().filter(Artist_Display_Name=artist_name)
 
+    similar_artists = {}
+    for x in Product.objects.all():
+        for y in products:
+            if x.Period == y.Period:
+                if len(similar_artists) < 3:
+                    similar_artists.update({x.Artist_Display_Name: x.image.url})
+
     context = {
         'products': products,
-        'artist': artist_name
+        'artist': artist_name,
+        'similar_artists': similar_artists,
     }
 
     return render(request, 'products/artist_detail.html', context)
