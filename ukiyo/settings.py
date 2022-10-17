2f5cebbcae2cd7879d161f52d6dbb009144640dd
David OConnell <51501850+daithioc95@users.noreply.github.com>
2022-10-17 16:33:46 +0100

    add railway url to allowed hosts settings.py

--- ukiyo/settings.py
+++ ukiyo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ms4-ukiyo-e', 'ms4-ukiyo-e.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['web-production-9793.up.railway.app/','ms4-ukiyo-e', 'ms4-ukiyo-e.herokuapp.com', 'localhost']
 
 
 # Application definition
